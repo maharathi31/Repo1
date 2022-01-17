5c72450505d1a53afdabb1bffd75d0e4772f1edb
maharathi31 <hriday.rathi@plaksha.edu.in>
2022-01-17 18:01:46 +0530

    added palindrome

---
+++ palindrom1.py
@@ -0,0 +1,12 @@
+def isPalindrome(string): 
+    for i in range(0, int(len(string)/2)): 
+        if string[i] != string[len(string)-i-1]:
+            return False
+    return True
+ 
+string = input("Please give a String : ")
+
+if (isPalindrome(string)):
+    print("Given String is a Palindrome")
+else:
+    print("Given String is not a Palindrome")
